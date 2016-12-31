349522f5a7d4cea0a4b2bd34c75948508170555b
theBestAndrew <theBestAndrew@users.noreply.github.com>
2016-12-31 14:28:11 -0500

    Initial commit generated from start.spring.io

---
+++ src/main/java/com/valiant_vaadin/VaadinWithSpringFromWebinarApplication.java
@@ -0,0 +1,12 @@
+package com.valiant_vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinWithSpringFromWebinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinWithSpringFromWebinarApplication.class, args);
+	}
+}
---
+++ src/test/java/com/valiant_vaadin/VaadinWithSpringFromWebinarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.valiant_vaadin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaadinWithSpringFromWebinarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
